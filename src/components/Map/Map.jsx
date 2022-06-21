d8efc40c4c08c927ca36e76498ef4840bb6ad93c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-06-21 12:51:22 -0400

    import requirements and styles for map.jsx'

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import googleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab'
+
+import useStyles from './styles';
+
 
 const Map = () => {
   return (
