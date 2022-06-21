e8d87aa52eef92ed48f7f4643e50bd9dca0de71f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-06-21 12:57:26 -0400

    add GoogleMapReact component and props

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,8 +8,25 @@ import useStyles from './styles';
 
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 0, lng: 0 }
+
   return (
-    <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: '' }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          options={''}
+          onChange={''}
+          onChildClick={''}
+          >
+        </GoogleMapReact>
+    </div>
   )
 }
 
