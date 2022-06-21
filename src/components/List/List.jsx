f7a0ad32052dae67c60e20c5901fd2790c34fa13
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-06-21 14:38:14 -0400

    add imports and styles to List component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
+import useStyles from './styles';
 
 const List = () => {
   return (
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import googleMapReact from "google-map-react";
+import GoogleMapReact from "google-map-react";
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab'
 
 import useStyles from './styles';
