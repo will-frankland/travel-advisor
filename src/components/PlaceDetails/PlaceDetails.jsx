38c79532c73a8edf98e562ad143529231182cd04
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-06-22 11:52:46 -0400

    add awards to restaurant cards inside PlaceDetails

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -18,6 +18,21 @@ const PlaceDetails = ({ place }) => {
       />
       <CardContent>
         <Typography gutterBottom variant="h5">{place.name}</Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Price</Typography>
+          <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Ranking</Typography>
+          <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+        </Box>
+        {place?.awards?.map((award) => (
+          <Box my={1} display="flex" justifyContent="space-between" alignItems="center"> 
+            <img src={award.images.small} alt={award.display_name} />
+            <Typography variant="subtitle2" color="textSecondary">{award.display_name}</Typography>
+          </Box>
+        ))}
+
       </CardContent>
     </ Card>
   )
