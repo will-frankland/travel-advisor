49f0f772e8e97b75c57efc2dd01629e086854b04
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-06-23 13:41:31 -0400

    minor bug fixes being found

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -14,7 +14,7 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
 
   return (
     <div className={classes.container}>
-      <Typography variant="h4">Food & Dining around you</Typography>
+      <Typography variant="h4">Dining Around You</Typography>
       {isLoading ? (
         <div className={classes.loading}>
           <CircularProgress size="5rem" />
@@ -30,7 +30,7 @@ const List = ({ places, type, setType, rating, setRating, childClicked, isLoadin
             </Select>
           </FormControl>
           <FormControl className={classes.formControl}>
-            <InputLabel id="rating">Rating</InputLabel>
+            <InputLabel id="rating">Select Rating</InputLabel>
             <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
               <MenuItem value="">All</MenuItem>
               <MenuItem value="3">Above 3.0</MenuItem>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,59 +8,57 @@ import useStyles from './styles';
 import mapStyles from './mapStyles';
 
 
-const Map = ( { coordinates, setBounds, setCoordinates, places, setChildClicked, weatherData }) => {
+const Map = ({ coordinates, setBounds, setCoordinates, places, setChildClicked, weatherData }) => {
   const classes = useStyles();
   const isDesktop = useMediaQuery('(min-width:600px)');
-  
+
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
-          defaultCenter={coordinates}
-          center={coordinates}
-          defaultZoom={14}
-          margin={[50, 50, 50, 50]}
-          options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
-          onChange={(e) => {
-            console.log('Here-->', e)
-            setCoordinates({ lat: e.center.lat, lng: e.center.lng });
-            setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
-          }}
-          onChildClick={(child) => {}}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, language: 'en' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+        }}
+        onChildClick={(child) => { setChildClicked(child) }}
+      >
+        {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
           >
-            {places?.map((place, i) => (
-              <div
-                className={classes.markerContainer}
-                lat={Number(place.latitude)}
-                lng={Number(place.longitude)}
-                key={i}
-                >
-                  {
-                    !isDesktop ? (
-                      <LocationOnOutlinedIcon color="primary" fontSize="large" />
-                    ) : (
-                      <Paper elevation={3} className={classes.paper}>
-                        <Typography className={classes.typography} variant="subtitle2" gutterBottom>
-                          {place.name}
-                          </Typography>
-                          <img
-                            className={classes.pointer}
-                            src={place.photo ? place.photo.images.large.url : ''}
-                            alt={place.name}
-                            />
-                            <Rating size="small" value={Number(place.rating)} readOnly />
-                      </Paper>
-                    )
-                  }
-              </div>
-            ))}
-            {weatherData?.list?.map((data, i) => (
-              <div key={i} lat={data.coordinates.lat} lng={data.coordinates.lon}>
-                <img height={100} src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} />
-              </div>
-            ))}
-        </GoogleMapReact>
+            {!isDesktop ?
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={place.photo ? place.photo.images.large.url : ''}
+                  alt={place.name}
+                />
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )
+            }
+          </div>
+        ))}
+        {weatherData?.list?.map((data, i) => (
+          <div key={i} lat={data.coordinates.lat} lng={data.coordinates.lon}>
+            <img height={100} src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} />
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   )
 }
