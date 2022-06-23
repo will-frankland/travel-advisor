188bf90bf044e4979baf2c25d34a13883fcf8817
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-06-23 12:24:48 -0400

    move google mpas api key to env file

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -16,7 +16,7 @@ const Map = ( { coordinates, setBounds, setCoordinates, places, setChildClicked,
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyBfB5zoTz54HY-WiVvrldB0Cc0xQNoB530' }}
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
           defaultCenter={coordinates}
           center={coordinates}
           defaultZoom={14}
